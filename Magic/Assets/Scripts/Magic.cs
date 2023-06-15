8f9a4221476eb1fb3db671eb4ba42f2d6845f6ce
georgeshea <52836004+GeorgeJShea@users.noreply.github.com>
2023-06-15 18:10:01 -0500

    Remapping of everythng
    github was failing to see the needed filed

---
+++ Magic/Assets/Scripts/Magic.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+
+
+public class Magic : MonoBehaviour
+{
+    /*
+        direction: the way the spell will go
+        assumes that is cast from self
+        target: 
+        shape: the shape of the spell
+        example: torrent ball explosion
+        target
+        element + / -
+
+
+
+        secondary effect
+        terrary effect
+     */
+
+    public GameObject spellObj;
+    public Vector3 direction;
+    public GameObject origin;
+    public string secondary;
+    public string smallEffect;
+    private MethodInfo method;
+
+    public string element;
+    public enum Element { water, fire , air, earth}
+    [SerializeField]public Element elli;
+
+    public enum Shape { ball, bolt}
+    [SerializeField] public Shape shapi;
+
+
+    public void Start()
+    {
+        //string[] temp = new string[] { "fireball", "water", "teal", "forward"};
+        //BuildSpell(temp);
+    }
+
+    public void CastSpell()
+    {
+        if(shapi == Shape.ball)
+        {
+            GameObject spell = Instantiate(spellObj, transform.position, Quaternion.identity);
+
+        }
+    }
+    public void BuildSpell(string[] methods)
+    {
+        for (int i = 0; i < methods.Length; i++)
+        {
+            //Debug.Log(methods[i]);
+            string temp = methods[i].ToUpper();
+            method = this.GetType().GetMethod(temp);
+
+            Debug.Log(i + "   " + methods[i].ToUpper() + "      " + method);
+
+
+            //FIREBAll();
+            try
+            {
+                method.Invoke(this, null);
+                Debug.Log("Somthing Accured");
+            }
+            catch
+            {
+                Debug.Log("Word unused for spell crafting: " + methods[i]);
+                // add addintonal information here for players
+            }
+        }
+    }
+
+    public void FORWARD()
+    {
+        direction = Vector3.forward;
+        Debug.Log("Direction Updated");
+    }
+
+    public void WATER()
+    {
+        Debug.Log("test");
+    }
+
+    public void FIREBALL()
+    {
+        FIRE();
+        BALL();
+    }
+
+    public void FIRE()
+    {
+        //element = "fire";
+        Instantiate(spellObj, transform.position, Quaternion.identity);
+        elli = Element.fire;
+        //Debug.Log("Fire fire fire");
+        // Element script goes here
+    }
+    public void BALL()
+    {
+        Debug.Log("ball");
+        // shape code here
+    }
+    
+}
---
+++ Magic/Assets/Scripts/SpeechToText.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.Windows.Speech;
+
+public class SpeechToText : MonoBehaviour
+{
+    // Speech Regnozier
+
+    public GameObject cube;
+    public GameObject magicSystem;
+
+    private DictationRecognizer dictRecognize;
+
+    public string spell = "";
+    private List<string> magicWords;
+    private List<string> magicReset;
+    Magic magic;
+
+    void Start()
+    {
+        magic = magicSystem.GetComponent<Magic>();
+        magicWords = new List<string>();
+        magicReset = magicWords;
+        dictRecognize = new DictationRecognizer();
+        dictRecognize.DictationHypothesis += Result;
+    }
+    void Update()
+    {
+        InputGather();
+
+        /*
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log("dead");
+            dictRecognize.Dispose();
+        }
+        */
+    }
+    private void InputGather()
+    {
+        /*
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Spell start");
+        }
+        if (Input.GetKey("a"))
+        {
+            dictRecognize.Start();
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+
+            dictRecognize.Stop();
+
+            Debug.Log("Spell Completed: " + spell + "|");
+            magicWords.Clear();
+        }
+
+        */
+    }
+    private void Result(string text)
+    {
+        if(text != null)
+        {
+            Debug.Log(" " + text + "   ");
+            //Debug.Log("Word appended");
+            magicWords.Add(text);
+            if (magicWords.Count != 0)
+            {
+                spell = magicWords[magicWords.Count - 1];
+                Debug.Log(spell);
+
+                magic.BuildSpell(magicWords.ToArray());
+            }
+            else
+            {
+                Debug.Log("REE");
+            }
+        }
+    }
+
+    private void processText(string text)
+    {
+
+    }
+
+}
+
